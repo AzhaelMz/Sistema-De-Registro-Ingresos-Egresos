6cab75f201085c10d6b5771c91afb747b03a33e0
AzhaelMz <158383050+AzhaelMz@users.noreply.github.com>
2024-08-13 12:06:02 -0500

    Add files via upload

---
+++ platisima-registros/src/main/java/com/platisima/platisima_registros/PlatisimaRegistrosApplication.java
@@ -0,0 +1,13 @@
+package com.platisima.platisima_registros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatisimaRegistrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatisimaRegistrosApplication.class, args);
+	}
+
+}
---
+++ platisima-registros/src/test/java/com/platisima/platisima_registros/PlatisimaRegistrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platisima.platisima_registros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatisimaRegistrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
